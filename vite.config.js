b7a0f08f6896574d4d2c6c57d3e5bdb78a43a283
Akbota Kaliollayeva <kaliollaeva.b@gmail.com>
2024-12-29 19:26:41 +0600

    deployed GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/tztechnodom/', // <-- Замените 'repository' на имя вашего репозитория
+  build: {
+    outDir: 'dist', // Папка для сборки
+  },
+});
+
